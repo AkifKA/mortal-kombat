e0a428590e78dc7d253d45c03ca07fc05c05637c
Mehmet Akif Karaöz <karaozmehmetakif@gmail.com>
2023-02-19 05:12:12 +0300

    data is creating 1

---
+++ src/assets/data.js
@@ -0,0 +1,96 @@
+export const data = [
+  {
+    name: "Raiden",
+    img:
+      "https://static.wikia.nocookie.net/mkwikia/images/c/cb/Ra.png/revision/latest/scale-to-width-down/350?cb=20200509154910",
+    desc:
+      "Raiden is a character in the Mortal Kombat fighting game series. Being the eternal God of Thunder, Raiden has been the one the most significant characters in the series along with being one of the most memorable characters in the fighting game genre. Raiden made his debut as one of the few original characters in the first Mortal Kombat game and is the one of the few characters to have appeared in every generation of Mortal Kombat games as a playable character. He has been a major, central character throughout the series.",
+    gender: "Male",
+    origin: "Heavens",
+    resides: "Earthrealm",
+    gif:
+      "https://static.wikia.nocookie.net/mkwikia/images/0/09/Rayden-mk2-fix.gif/revision/latest?cb=20081004201213",
+    species: "1st Timeline: God.2nd Timeline: Human (Formerly God)",
+  },
+
+  {
+    name: "Goro",
+    img:
+      "https://static.wikia.nocookie.net/mkwikia/images/7/74/GoroRenderMKX.png/revision/latest?cb=20150506005824",
+    desc:
+      "Goro is a character in the Mortal Kombat fighting game series. The Prince of the Shokan and former champion of the Mortal Kombat. Tournament, he is one of the most recognizable and memorable characters in the series. He is a frequent, recurring antagonist in the series and major ally to the sorcerer Shang Tsung.",
+    gender: "Male",
+    origin: "Outworld",
+    resides: "Outworld",
+    gif:
+      "https://static.wikia.nocookie.net/mkwikia/images/6/66/GordanceYEA%21.gif/revision/latest?cb=20091108190157",
+    species: "Shokan",
+  },
+
+  {
+    name: "Johnny Cage",
+    img:
+      "https://static.wikia.nocookie.net/mkwikia/images/6/66/Johnny_Render_mk_11_1.png/revision/latest/scale-to-width-down/343?cb=20190227213436",
+    desc:
+      "Jonathan Carlton, better known by his stage name Johnny Cage, is an action movie star and martial artist in the Mortal Kombat fighting game series. He is one of the seven original characters, debuting in the first Mortal Kombat arcade game, and remains one of the most popular characters in the franchise to date.Johnny Cage has been a central character to the series alongside the likes of Raiden and Liu Kang, a role that was later passed down to his daughter, Cassie Cage, as well.",
+    gender: "Male",
+    origin: "Earthrealm (Venice, California, USA)",
+    resides: "Earthrealm",
+    gif:
+      "https://static.wikia.nocookie.net/mkwikia/images/d/db/Jcmk2.gif/revision/latest?cb=20091003212807",
+    species: "Human",
+  },
+  {
+    name: "Kano",
+    img:
+      "https://static.wikia.nocookie.net/mkwikia/images/6/67/Kano_MK11.png/revision/latest/scale-to-width-down/350?cb=20190227221749",
+    desc:
+      "Kano (加納)[2] is a character in the Mortal Kombat fighting game series. The leader of the Black Dragon clan, he is a ruthless scoundrel and archnemesis to Sonya Blade and the Special Forces.Kano made his debut as one of the few original characters in the first Mortal Kombat arcade game. He is the first Black Dragon clan member featured in the series and has been a recurring antagonist to the franchise since his debut. He serves as a major antagonist towards Sonya Blade and her fam",
+    gender: "Male",
+    origin: "Earthrealm (Australia)",
+    resides: "Outworld",
+    gif:
+      "https://static.wikia.nocookie.net/mkwikia/images/4/45/Kano-r.gif/revision/latest?cb=20081004230255",
+    species: "Human",
+  },
+  {
+    name: "Liu Kang",
+    img:
+      "https://static.wikia.nocookie.net/mkwikia/images/0/00/Liu_Kang_mk11.png/revision/latest/scale-to-width-down/350?cb=20190329012026",
+    desc:
+      "Liu Kang is a character in the Mortal Kombat fighting game series. He is one of the few original characters, debuting in the first Mortal Kombat arcade game. He serves as the protagonist of Mortal Kombat, Mortal Kombat II, Mortal Kombat 3, Mortal Kombat 4, Mortal Kombat (2011) (co-protagonist), Mortal Kombat 11, the live-action films and the original comic book series. He became the Grand Champion of Mortal Kombat throughout the first four tournaments, a title that remained undisputed in the original timeline. In the alternate timeline, he only became the Grand Champion on two occasions, due to the fact that a third tournament did not take place during the invasion of Earthrealm. Although Liu Kang appears in every main Mortal Kombat game, he is not playable in Mortal Kombat: Deadly Alliance, as he was killed by the Deadly Alliance.",
+    gender: "Male",
+    origin: "Earthrealm (Henan Province, China)",
+    resides: "1st Timeline: Earthreal. 2nd Timeline: Chaotian Age",
+    gif:
+      "https://static.wikia.nocookie.net/mkwikia/images/b/b8/Liu_Kang_MK3.gif/revision/latest?cb=20110422221715",
+    species:
+      "1st Timeline: Zombie/Spirit.2nd Timeline: God (formerly Human/Revenant)",
+  },
+  {
+    name: "Noob Saibot",
+    img:
+      "https://static.wikia.nocookie.net/mkwikia/images/b/b0/Noob_Saibot_mk11.png/revision/latest?cb=20190322210711",
+    desc:
+      "Noob Saibot, once known as Bi-Han (壁寒)[1][2] and the Elder Sub-Zero, is a Lin Kuei assassin turned wraith in the Mortal Kombat fighting game series.Bi-Han made his debut as one of the few original characters in the first Mortal Kombat game as the original Sub-Zero, a mantle later passed down to his younger brother Kuai Liang after his death. He later reappears as the Elder Sub-Zero in Mortal Kombat (2011).",
+    gender: "Male",
+    origin: "Earthrealm (China).(reborn in the Netherrealm)",
+    resides: "Netherrealm",
+    gif:
+      "https://static.wikia.nocookie.net/mkwikia/images/8/8b/NOOB.gif/revision/latest?cb=20090527012107",
+    species: "Wraith (formerly Human/Cryomancer)",
+  },
+  {
+    name: "Reptile",
+    img:
+      "https://static.wikia.nocookie.net/mkwikia/images/4/4a/Reptile_Render.png/revision/latest/scale-to-width-down/350?cb=20150407150943",
+    desc:
+      "Syzoth, more commonly referred to as Reptile, is a character in the Mortal Kombat fighting game series. He is one of the few original characters, debuting in the original Mortal Kombat arcade game as a secret character. He made his playable debut in Mortal Kombat II.Since his debut, Reptile has been a well known servant to the Outworld Empire, faithfully serving the Outworld Emperors Shao Kahn and Kotal Kahn while remaining loyal to his master Shang Tsung.",
+    gender: "Male",
+    origin: "Outworld",
+    resides: "Saurian",
+    gif:
+      "https://static.wikia.nocookie.net/mkwikia/images/8/8b/NOOB.gif/revision/latest?cb=20090527012107",
+    species: "Wraith (formerly Human/Cryomancer)",
+  },
+];
